e73fe544b449d16b5d6d54e7b5a103e98139a9fd
LuisDev <lurojas@ucm.es>
2020-04-05 20:09:32 +0200

    Fin del proyecto

--- noticias/src/Components/Noticias.js
+++ noticias/src/Components/Noticias.js
@@ -4,12 +4,19 @@ const Noticias = ({noticia}) => {
     //extraer los datos
 
     const { urlToImage,url,title,description,source} = noticia;
+
+    //si no hay imagen 
+    const imagen = (urlToImage) ?
+    <div className="card-image">
+        <img src={urlToImage} alt ={title}/>
+        <span className="card-title">{source.name}</span>
+    </div>
+    :null;
+
     return (
         <div className="col s12 m6 l4">
            <div className="card">
-               <div className="card-image">
-                <img src={urlToImage} alt ={title}/>
-               </div>
+               {imagen}
 
                 <div className="card-content">
                      <h3>{title}</h3>
