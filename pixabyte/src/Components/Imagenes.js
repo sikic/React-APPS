02c098866f7683467dc46f22aca0d0c8254b4e3b
LuisDev <lurojas@ucm.es>
2020-04-07 21:29:02 +0200

    mejora 1: imágenes iguales sin perder calidad

--- pixabyte/src/Components/Imagenes.js
+++ pixabyte/src/Components/Imagenes.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import'./Imagenes.css'
 const Imagenes = ({actual}) => {
 
     const {largeImageURL,previewURL,likes,tags,views} = actual;
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
+                <div className="content">
                  <img src={previewURL} alt ={tags} className="card-img-top" />
+                </div>
 
                 <div className="card-body">
                     <p className="card-text">
