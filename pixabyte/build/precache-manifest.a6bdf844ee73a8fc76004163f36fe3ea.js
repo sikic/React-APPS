3f4265f1590f98653cf906fa8aca5c58decbbce0
LuisDev <lurojas@ucm.es>
2020-04-08 19:16:54 +0200

    Inicio proyecto de buscador de letras de canciones

---
+++ pixabyte/build/precache-manifest.a6bdf844ee73a8fc76004163f36fe3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edceb5edcc9dd21821e6fd74152dcea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fa7699f1d77af19a177",
+    "url": "/static/css/main.d3266dc5.chunk.css"
+  },
+  {
+    "revision": "8b3947d13f50d01dc5b0",
+    "url": "/static/js/2.32c79eca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32c79eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa7699f1d77af19a177",
+    "url": "/static/js/main.342b22ed.chunk.js"
+  },
+  {
+    "revision": "a54ac19a459f46791cd0",
+    "url": "/static/js/runtime-main.45f3accb.js"
+  }
+]);
