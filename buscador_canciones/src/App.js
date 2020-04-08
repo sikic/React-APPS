54856e0a611b08849ab3ced3bc93ac13409c81d6
LuisDev <lurojas@ucm.es>
2020-04-08 19:45:54 +0200

    parte 1

--- buscador_canciones/src/App.js
+++ buscador_canciones/src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment,useState} from 'react';
+import Formulario from './Components/Formulario'
 
 function App() {
+  const [terminosBusqueda, setterminosBusqueda] = useState({
+    canc
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario/>
+    </Fragment>
   );
 }
 
---
+++ buscador_canciones/src/Components/Formulario.js
@@ -0,0 +1,86 @@
+import React,{useState} from 'react'
+
+const Formulario = () => {
+//state
+    const [busqueda, setbusqueda] = useState({
+        artista:'',
+        cancion:''
+    })
+
+    const [error, seterror] = useState(false);
+    //funcion de cada state para leer su input
+    const actualizarState = e =>{
+        setbusqueda({
+           ...busqueda,
+           [e.target.name] :e.target.value 
+        })
+    }
+
+
+    const {artista,cancion} = busqueda;
+
+    const buscarInformarcion= e=>{
+        e.preventDefault();
+
+        //validamos
+        if(artista.trim() ==='' || cancion.trim() === '')
+            return seterror(true);
+
+        seterror(false);
+
+        //pasar al componente ppal
+    }
+    return (
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                     onSubmit={buscarInformarcion}
+                     className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+                        <fieldset>
+                            <legend className="texte-center">Buscador letras canciones</legend>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artista:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artista"
+                                            value={artista}
+                                            placeholder="Nombre artista"
+                                            onChange={actualizarState}
+                                        />
+                                    </div>
+
+                                </div>
+
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Canción:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            value={cancion}
+                                            placeholder="Nombre canción"
+                                            onChange={actualizarState}
+                                        />
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <button 
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >Buscar</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Formulario
