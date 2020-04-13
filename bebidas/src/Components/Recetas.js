67b742b79c3078f256a1b90dd544038076d7d3ed
LuisDev <lurojas@ucm.es>
2020-04-13 19:00:42 +0200

    Mejora: scroll en el modal

--- bebidas/src/Components/Recetas.js
+++ bebidas/src/Components/Recetas.js
@@ -2,6 +2,7 @@ import React,{useContext, createContext,useState} from 'react'
 import {ModalContext} from'../Context/ModalContext'
 import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
+import '../index.css'
 
 
 function getModalStyle() {
@@ -21,7 +22,8 @@ const useStyles = makeStyles(theme => ({
       width: 300,
       backgroundColor: theme.palette.background.paper,
       boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
+      padding: theme.spacing(2, 2, 3),
+      
     },
 }));
 
@@ -49,14 +51,16 @@ const Recetas = ({item}) => {
 
     const mostrarIngredientes = (informacion) =>{
         let Ingredientes = [];
-        for (let index = 1; index < 16; index++) {
-           if(informacion[`strIngredients${index}`] !== null)
+        let encontrado = false;
+        for (let index = 1; index < 16 & !encontrado; index++) {
+           if(informacion[`strIngredient${index}`] !== null)
                 Ingredientes.push(<li> {informacion[`strIngredient${index}`] }
                  { informacion[`strMeasure${index}`]}
                 </li>);
-            
+            else
+                encontrado=true;
         }
-
+        console.log(Ingredientes);
         return Ingredientes;
     }
     return (
@@ -86,18 +90,21 @@ const Recetas = ({item}) => {
                         }}
                     >
                         <div style={modalStyle} className={classes.paper}>
-                            <h2>{detalles.strDrink}</h2>
-                            <h3 className="mt-4">Instrucciones</h3>
-                            <p>
-                               {detalles.strInstructions} 
-                            </p>
+                            <div className="scroll">
 
-                            <img className="img-fluid my-4" src={detalles.strDrinkThumb} />
+                                <h2>{detalles.strDrink}</h2>
+                                <h3 className="mt-4">Instrucciones</h3>
+                                <p>
+                                {detalles.strInstructions} 
+                                </p>
 
-                            <h3>Ingredientes y cantidades</h3>
-                            <ul>
-                                {mostrarIngredientes(detalles)}
-                            </ul>
+                                <img className="img-fluid my-4" src={detalles.strDrinkThumb} />
+
+                                <h3>Ingredientes y cantidades</h3>
+                                <ul>
+                                    {mostrarIngredientes(detalles)}
+                                </ul>
+                            </div>
                         </div>
                     </Modal>
                 </div>
