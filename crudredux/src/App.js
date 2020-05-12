e856d78612290888fb67cf26ef731d3ca94945a3
LuisDev <lurojas@ucm.es>
2020-05-12 18:12:01 +0200

    parte 2

--- crudredux/src/App.js
+++ crudredux/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Header from './components/Header'
+import { BrowserRouter as Router , Route,Switch} from 'react-router-dom'
+import Productos from './components/Productos'
+import NuevoProducto from './components/NuevoProducto'
+import EditarProducto from './components/EditarProducto'
 
 
 function App() {
   return (
-   <h1>
-     Que pasa
-   </h1>
+   <Router>
+     <Header/>
+
+     <div className ="container mt-5">
+        <Switch>
+          <Route exact path="/" component= {Productos} />
+          <Route exact path="/productos/nuevo" component= {NuevoProducto} />
+          <Route exact path="/productos/editar/:id" component= {EditarProducto} />
+
+        </Switch>
+     </div>
+   </Router>
   );
 }
 
